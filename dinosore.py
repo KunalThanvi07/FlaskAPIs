aa2a98760a77dd0f53e7c10e91f166c81a5951ce
KunalThanvi07 <98839373+KunalThanvi07@users.noreply.github.com>
2024-08-20 11:18:08 +0530

    Add files via upload

---
+++ dinosore.py
@@ -0,0 +1,11 @@
+import pyautogui
+import time 
+pyautogui.FAILSAFE = False
+
+while True :
+    time.sleep(300) # give 300 in sleep time
+    for i in range (0, 100):
+        pyautogui.moveTo(0, i * 5)
+
+    for i in range (0,3):
+        pyautogui.press('shift')    
